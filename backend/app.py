ecca1af9a0d3b00bea646e4df1c8768fe21c3eec
Aryan Kadam <aryankadam3114@gmail.com>
2025-01-29 23:11:48 +0530

    Updated Requirements

--- backend/app.py
+++ backend/app.py
@@ -28,4 +28,9 @@ def upload_image():
     return jsonify({"caption": caption})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    import os
+
+port = os.environ.get("PORT", "5000")  # Default to 5000 if PORT is not set
+app.run(debug=True, host="0.0.0.0", port=int(port))
+
+
