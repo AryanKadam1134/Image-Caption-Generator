d4246ef98847bd1c62b0552a0ddc75f4f913ae03
Aryan Kadam <aryankadam3114@gmail.com>
2025-01-29 23:54:32 +0530

    Hosted Backend

--- image-caption-generator/src/App.js
+++ image-caption-generator/src/App.js
@@ -23,7 +23,7 @@ function App() {
 
     try {
       setLoading(true);
-      const response = await axios.post("http://localhost:5000/upload", formData, {
+      const response = await axios.post("https://image-caption-generator-3.onrender.com", formData, {
         headers: { "Content-Type": "multipart/form-data" },
       });
       setCaption(response.data.caption);
